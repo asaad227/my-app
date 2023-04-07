6878e6af7a9e91dd344c9d342f2bca643966e10b
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-04-07 23:16:16 +0100

    get, post and delete requested added to the list through mongoDb connection and netlify function

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,40 +2,49 @@ import React, { useEffect, useState} from 'react';
 import ListItem from '../ListItem/ListItem';
 import Placeholder from '../Placeholder/Placeholder';
 import './List.css'
-import PostTodo from '../../lib/data/list';
+
 
 // import Footer from '../Footer/indx';
 
 export default function List() {
     const [listInput, setListInput] = useState("");
     const [val, setVal] = useState("");
-    const [txt, setTxt] = useState();
+    const [txt, setTxt] = useState([]);
     const [show, setShow] = useState();
-    const [list, setList] = useState()
+    const [list, setList] = useState([])
  
-console.log(list.text);
-    function addToList() {
-        let newList=[];
+async function getApi(){
+    let response = await fetch(`/.netlify/functions/get_todoList`, {
+        method: "GET",
+      });
+      let data = await response.json();
+     setList(data)
+}
+
+async function fetchPostTodo() {
+    let response = await fetch(`/.netlify/functions/post_todoList`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({text:listInput[0].toUpperCase()+listInput.slice(1), time:val, checked:false}),
+    });
+    let data = await response.json();
+    console.log("post data", data);
+  }
+
+
+function addToList(index) {
+    //as this will send the list to the database so fetchPostTodo execute here
+        fetchPostTodo()
         // Output: Sat Apr 01 2023 01:53:25
-        const todo = listInput[0].toUpperCase()+listInput.slice(1)
-        const newListItem = {
-            text: todo,
-            date:val,
-            checked: false
-        }
-        if (list === null || list.length === 0) {
-            newList = [];
-        } else {
-            newList = [...list];
-        }
         //this if clause not going to allow anymore txt to added to the list when list
         // less then 6.
        if(list.length > 5){
        alert("Maximum entry entered. Please delete one then add one!!")
        }else{
-        newList.push(newListItem);
-        <PostTodo todo={newList}/>;
-        setList(newList);
+        setTxt({text:listInput[0].toUpperCase()+listInput.slice(1), time:val, checked:false});
         setListInput("")
        }
            
@@ -44,90 +53,45 @@ console.log(list.text);
        
     }
 
-    function removeFromList(index) {
+    async function removeFromList(index) {
         //spreeding the list
         const newList = [...list]
         // newList index not equal to checked 
         newList[index].checked = !newList[index].checked;
-        setList(newList)
+        let response = await fetch(`/.netlify/functions/delete_todoList`, {
+            method: "DELETE",
+            body: JSON.stringify({_id:txt[index]}),
+          });
+          let data = await response.json();
+          console.log("delete data", data);
+        setTxt(newList)
         //useing setTimeout to clear the checkbox after 1sec
         setTimeout(() => {
             const finalList = newList.filter(el => !el.checked);
-            localStorage.setItem('toDoList', JSON.stringify(finalList));
-            setList(finalList);
+            setTxt(finalList);
+           
         }, 1000)
+
+        setTimeout(() => {
+           getApi()
+           
+        }, 2000)
     }
     function checklist() {
         if (list === null || list.length === 0) {
             return <Placeholder/>
         } else {
-            return  list.map((item, index) => <ListItem checkStatus={item.checked} deleteHandler={removeFromList} index={index} key={index} text={item.text} dateView={item.date}></ListItem>)
+            return  list.map((item, index) => <ListItem checkStatus={item.checked} deleteHandler={removeFromList} index={index} key={index} text={item.text} dateView={item.time}></ListItem>)
         }
     }
 
     
   useEffect(() => {
- if(list === null || list.length === 0){
-    checklist()
- }else{
-    updateItem()
- }
-  },)
+    getApi()
+ 
+  },[])
   
-    
-
-   
-function updateItem(){
-    let currentDate = new Date();
-    // casting the dateobject to use general purpose.
-    let day = currentDate.toLocaleDateString();
-    let time=currentDate.toLocaleTimeString();
-   let year = day.slice(6, 10);
-   let dated1 = day.slice(0, 2);
-   let month = day.slice(3, 5);
-   let hrsMin = time.slice(0,5);
-   let hrs = time.slice(0,2);
-   let firstMin = time.slice(3,4)
-   let min =Number(time.slice(4,5)) + Number(1);
-   var newHrsMin=`${hrs}:${firstMin+min}`
-   console.log(newHrsMin,firstMin);
-   //casting from day and time to year, month, day, hrs and minutes to put into variable 
-   //to comare with currentime to due date variable.
-   let timeV = `${year}-${month}-${dated1}T${hrsMin}`
-   let timeV2 = `${year}-${month}-${dated1}T${newHrsMin}`
-
-    for(let i = 0; i < list.length; i++){
-        //compare time now minutes and due date setup time minutes 
-        
-        if(list[i].date === timeV2 ){
-            setTxt(`You have one minute to complete your ${list[i].text} task hurry up!!!`);
-         }else{
-             setTxt("")
-         }
-       
-        if(list[i].date === timeV){
-            //if match list checked property will change to true
-            list[i].checked = true;
-          
-          //need to update local storage according to recent update 
-          // so need to spreed the list then filter out checked item
-          // then sent new list to the localstorage
-          // and also update setList with final list
-            const newList =[...list]
-            setTimeout(() => {
-                const finalList = newList.filter(el => !el.checked);   
-                localStorage.setItem('toDoList', JSON.stringify(finalList));
-                setList(finalList);
-            }, 2000)
-        }
-       
-       
-        
-}
-                       
-    }
-  
-      
+      console.log("I am list", list);
    
     return (
         <div className="List">
@@ -136,11 +100,12 @@ function updateItem(){
       <div className="todoItems">
         
         { checklist() }
+        
        
       </div>
-      <div>
+      {/* <div>
       <p className='warning'>{txt}</p>
-      </div>
+      </div> */}
      
    
       <form className={!show?"todoInput":"todoHide"} >
--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,19 +1,12 @@
 import './ListItem.css';
 
 export default function ListItem({text, index, checkStatus, deleteHandler, dateView}) {
-function dateAndTime(){
-    //this will checked if user has provided date and time or not
-    if(dateView === ""){
-        return null
-    }else{
-        return `Due by: ${dateView}`;
-    }
-}
+
 
 
     return (
     <ul className='ul-list'>
-        <li><input type="checkbox" checked={checkStatus} onChange={deleteHandler.bind(null, index)} placeholder='checkbox'/><div className='textItem'><p>{text}</p> <p className='timeStamp'> {dateAndTime()}</p></div></li>
+        <li><input type="checkbox" checked={checkStatus} onChange={deleteHandler.bind(null, index)}/><div className='textItem'><p>{text}</p> <p className='timeStamp'> {dateView}</p></div></li>
         </ul>
       
     )
