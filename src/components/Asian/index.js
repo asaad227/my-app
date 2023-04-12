9bd099bdae0639e3cfcfe7d7038e4d51a075d238
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-04-12 09:35:04 +0100

    update asian recipe fetch function and fav section function

--- src/components/Asian/index.js
+++ src/components/Asian/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './index.css';
 import { MdOutlineFavoriteBorder } from "react-icons/md";
 import Menu from '../Menu';
@@ -10,34 +10,36 @@ function Asian() {
   const [cuisineInput, setCuisineInput] = useState('');
   const [toggle, setToggle] = useState(false)
   const [data, setData] = useState(() => {
-    const storedList = localStorage.getItem("recipe");
+    const storedList = localStorage.getItem("myRecipe");
     return storedList ? JSON.parse(storedList) : [];
   });
 
- 
- 
-  async function testFetch(){
+ useEffect(()=>{
+     localStorage.setItem("myRecipe", JSON.stringify(data))
+ },[data])
+ async function getApi(){
    
-    const responce = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${dishInput}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&cuisineType=${cuisineInput}&random=true`);
-    const data = await responce.json();
-    if(data.status === 404){
-      console.log("error");
-    }else {
-      // adding id to the api and adding 1+ index number
-      const {hits} = data;
-      const exist = await hits.map((e, i)=> e? {...e, id:Number(1)+i}: e);
-      //setting local storage with this new api
-      localStorage.setItem("myRecipe", JSON.stringify(exist))
+  const responce = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${dishInput}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&cuisineType=${cuisineInput}&random=true`);
+  const data = await responce.json();
+  if(data.status === 404){
+    console.log("error");
+  }else {
+    // adding id to the api and adding 1+ index number
+    const {hits} = data;
+    const exist = await hits.map((e, i)=> e? {...e, id:Number(1)+i}: e);
+    //setting local storage with this new api
+    localStorage.setItem("myRecipe", JSON.stringify(exist))
 
-      console.log(exist)
-      setData(exist)
+    console.log(exist)
+    setData(exist)
+  }
+  
     }
-      }
+  
     //  testFetch()
   function onSubmit(e){
     e.preventDefault();
-    testFetch()
-    
+    getApi();
   } 
   function display(e){
     e.preventDefault();
@@ -45,12 +47,12 @@ function Asian() {
       
     }
 
-function fav(){
+function fav(index){
 const list = [...data];
-list.id = !list.id
+list[index].id = !list[index].id
 
 const finalList = list.filter(e=> !e.id)
-
+localStorage.setItem("myFav", JSON.stringify(finalList))
 console.log(finalList);
 }
  console.log(data);
@@ -79,7 +81,7 @@ console.log(finalList);
 <img src={e.recipe.image} width={200} height={200} alt={e.recipe.label}/>
 <h6 style={{color:'gold'}}>Meal type: {e.recipe.mealType}</h6>
 </div>
-<button onClick={fav} className="reBtn"><MdOutlineFavoriteBorder className='iconNav' /></button>
+<button key={index} onClick={()=>fav(index)} className="reBtn"><MdOutlineFavoriteBorder className='iconNav' /></button>
 
 <form onSubmit={display}>
   <p  className='ingredientPara'>{toggle? e.recipe.ingredientLines: ''}</p>
--- src/components/Fav/Fav.js
+++ src/components/Fav/Fav.js
@@ -4,44 +4,33 @@ import { RiDeleteBin5Line } from "react-icons/ri";
 import Menu from '../Menu';
 
 export default function Fav() {
-  const [data, setData] = useState([])
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState({})
+  const [data, setData] = useState(() => {
+    const storedList = localStorage.getItem("myFav");
+    return storedList ? JSON.parse(storedList) : [];
+  });
 
-  function newData(){
-    if(localStorage.length >= 1){
-      const data = Array.from(Object.values(localStorage))
-      const e =[]
-      for(let i = 0; i < localStorage.length; i++){
-         e.push( JSON.parse(data[i]))
-      }
-    setData(e)
+ useEffect(()=>{
+     localStorage.setItem("myFav", JSON.stringify(data))
+ },[data])
+  
+
+  function remove(index){
+    const list = [...data];
+    list[index].id = !list[index].id
+    const finalList = list.filter(e=> !e.id)
+    localStorage.removeItem("myFav", JSON.stringify(finalList))
+   setData(finalList)
+
     }
-  }
-useEffect(()=>{
-  newData()
-},[])
 
-function remove(e){
-      setData(data.filter((x) => x.label !== e.label));
-      localStorage.removeItem(e.label)
-      
+const display=function(e){
+  e.preventDefault()
 }
 
-// function display(e){
-//   const ex = data.find((x)=> x.label === e.label);
-  
-//   console.log('check',ex.image, data)
 
-//   if(ex){
-//    setData(data.map((x)=> x.label === e.label? x.label.ingredientLines: x))
-//   }
-// }
 
-function onSubmit(e){
-e.preventDefault();
- setToggle(!toggle)
-  
-} 
+
 
 
    
@@ -52,20 +41,20 @@ e.preventDefault();
   return (<div className='App'>
   <Menu/>
   <div className='flex-container'>
-  {data?.map((e, index)=> <section className='flex-boxFav' key={index}><h4 className='label'>{e.label}</h4>
-  <div>
-  <img src={e.image} width={200} height={200} alt={e.label}/>
-  <h6 style={{color:'gold'}}>Meal type: {e.mealType}</h6>
-  </div>
-  <form onSubmit={onSubmit}>
-  <p  className='ingredientPara'>{toggle? e.ingredientLines: ''}</p>
-  <button className='cuisineInput'>{!toggle? 'Get Ingredient': 'Hide Ingredient'}</button>
+  {data.map((e, index)=> <section className='flex-box' key={index}><h4 className='label'>{e.recipe.label}</h4>
+<div>
+<img src={e.recipe.image} width={200} height={200} alt={e.recipe.label}/>
+<h6 style={{color:'gold'}}>Meal type: {e.recipe.mealType}</h6>
+</div>
+<button key={index} onClick={()=>remove(index)} className="reBtn"> <RiDeleteBin5Line/></button>
+
+<form onSubmit={()=>display(index)}>
+  <p key={index}  className='ingredientPara'>{toggle? e.recipe.ingredientLines: ''}</p>
+  <button key={index} className='cuisineInput'>{!toggle? 'Get Ingredient': 'Hide Ingredient'}</button>
   </form>
-
-  <h6 style={{color:'gold'}}>Total time: {e.totalTime} min</h6>
-
-  <button onClick={()=>remove(e)} className='reBtn'><RiDeleteBin5Line className='iconNav' /></button>
-  </section>)}
+ 
+<h6 style={{color:'gold'}}>Total time: {e.recipe.totalTime} min</h6>
+</section>)}
   </div>
   </div>
   )
