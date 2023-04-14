4bee9040a0472f9b5052cd923ca4371edc54ae99
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-04-14 11:23:35 +0100

    timestamp color change

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState, useCallback, lazy } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import ListItem from '../ListItem/ListItem';
 import Placeholder from '../Placeholder/Placeholder';
 import './List.css'
